eb0bff211da89c216d419a0e811c9a16d3e78c22
EihabAhmed <eihabahmed@gmail.com>
2022-12-29 07:16:00 +0200

    Completed Order Service (01:05:45)

---
+++ order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
@@ -0,0 +1,42 @@
+package com.programmingtechie.orderservice.service;
+
+import com.programmingtechie.orderservice.dto.OrderLineItemDto;
+import com.programmingtechie.orderservice.dto.OrderRequest;
+import com.programmingtechie.orderservice.model.Order;
+import com.programmingtechie.orderservice.model.OrderLineItem;
+import com.programmingtechie.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItem> orderLineItems = orderRequest.getOrderLineItemDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemList(orderLineItems);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItem mapToDto(OrderLineItemDto orderLineItemDto) {
+        OrderLineItem orderLineItem = new OrderLineItem();
+        orderLineItem.setPrice(orderLineItemDto.getPrice());
+        orderLineItem.setQuantity(orderLineItemDto.getQuantity());
+        orderLineItem.setSkuCode(orderLineItemDto.getSkuCode());
+        return orderLineItem;
+    }
+}
